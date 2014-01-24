615bc43be4ef41aa598a193c9d4f67998d26e880
svolochev <svolochev@yandex.ru>
2014-01-24 14:01:17 +0600

    Version 1.5.2: * Floating-point numbers support has been added.

--- ddPadej.php
+++ ddPadej.php
@@ -1,20 +1,27 @@
 <?php
 /**
  * ddPadej.php
- * @version 1.5.1 (2012-08-09)
+ * @version 1.5.2 (2013-09-11)
  * 
  * @desc The snippet writes correct inflexions for the words connecting with numerals. 
  * 
  * @param str {comma separated string} - String with inflexions separated by commas “v1,v2,v3”. Where “v1” — inflexion for 10; “v2” — for 1; “v3” — for 2. @required
- * @param n {integer} - Evaluating number. @required
+ * @param n {float} - Evaluating number. @required
  * 
- * @link http://code.divandesign.ru/modx/ddpadej/1.5.1
+ * @link http://code.divandesign.ru/modx/ddpadej/1.5.2
  * 
- * @copyright 2012, DivanDesign
+ * @copyright 2013, DivanDesign
  * http://www.DivanDesign.biz
  */
 
 $str = explode(',',$str);
+
+preg_match('/^(.*)(\,|\.)(.*)$/', strval($n), $matches);
+
+if(!empty($matches)){
+	return $str[2];
+}
+
 $mod = $n % 100;
 if (($mod >= 10) && ($mod <=20)) return $str[0];
 else{
