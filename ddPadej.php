1786f8223d6f86a36baf23911f0579660246c21a
svolochev <svolochev@yandex.ru>
2014-01-24 13:56:45 +0600

    Version 1.5.1: * “split” has been replaced by “explode” for PHP 5.3 compatibility.

--- ddPadej.php
+++ ddPadej.php
@@ -1,20 +1,20 @@
 <?php
 /**
  * ddPadej.php
- * @version 1.5 (2010-05-21)
+ * @version 1.5.1 (2012-08-09)
  * 
  * @desc The snippet writes correct inflexions for the words connecting with numerals. 
  * 
  * @param str {comma separated string} - String with inflexions separated by commas “v1,v2,v3”. Where “v1” — inflexion for 10; “v2” — for 1; “v3” — for 2. @required
  * @param n {integer} - Evaluating number. @required
  * 
- * @link http://code.divandesign.ru/modx/ddpadej/1.5
+ * @link http://code.divandesign.ru/modx/ddpadej/1.5.1
  * 
  * @copyright 2012, DivanDesign
  * http://www.DivanDesign.biz
  */
 
-$str = split(',',$str);
+$str = explode(',',$str);
 $mod = $n % 100;
 if (($mod >= 10) && ($mod <=20)) return $str[0];
 else{
